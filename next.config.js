67679d745c1228c8ccdcc2c83ca51b44f181651e
herlan-syihabudin <herlansyihabudin@gmail.com>
2026-05-25 09:23:04 +0700

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,26 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['images.unsplash.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  swcMinify: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production',
+  },
+}
+
+module.exports = nextConfig
